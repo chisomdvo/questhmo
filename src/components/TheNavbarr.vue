06276b462beb29ef195cf89e254165df9bca0747
chisom ibeku <chuck.ibeku@gmail.com>
2023-03-31 06:29:33 +0100

    changed get started router link

--- src/components/TheNavbarr.vue
+++ src/components/TheNavbarr.vue
@@ -176,7 +176,7 @@
                 LOGIN
               </router-link>
               <router-link
-                to="/home"
+                to="/"
                 class="ml-8 whitespace-nowrap cursor-pointer inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
               >
                 GET STARTED
